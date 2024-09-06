796a2fab4e15b354f61c34ab50f84f0f76a444fb
Joselyn-Alejandra-Valenzuela-0412 <164534000+Joselyn-Alejandra-Valenzuela-0412@users.noreply.github.com>
2024-09-06 12:16:33 -0600

    Add files via upload
    subi archivo, listas, tuplas, conjuntos y diccionario

---
+++ conjuntos.py
@@ -0,0 +1,5 @@
+mistilinas={"alondra","johana"}
+print(mistilinas)
+print("--loop--")
+for nombrest in mistilinas:
+    print(nombrest)
---
+++ diccionario.py
@@ -0,0 +1,11 @@
+alumna = {
+    "ID": 22308051280412,
+"nombre": "Joselyn Alejandra Valenzuela",
+"especialidad": "programacion",
+"edad": 18,
+"correo": "a.21308051280552@cbtis128.edu.mx"
+}
+print(alumna)
+print("--diccionario info--")
+for infomia in alumna:
+    print(alumna[infomia])
